e1ef6031c4f11f4a8bcea5378f13847b4bf39a4c
Sandra Kao/WIH/Wistron <Sandra_Kao@wistron.com>
2021-10-08 10:24:30 +0800

    Hero List and Details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { HEROES } from './../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 @Component({
@@ -6,13 +7,14 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'LiChen'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSelect(hero: Hero): void {
 
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'LiChen' },
+  { id: 11, name: 'Dory' },
+  { id: 12, name: 'Jayson' },
+  { id: 13, name: 'Irene' },
+  { id: 14, name: 'Brad' },
+  { id: 15, name: 'Scott' },
+  { id: 16, name: 'Chris' },
+  { id: 17, name: 'Iris' },
+  { id: 18, name: 'Sandra' },
+  { id: 19, name: 'Steve' },
+  { id: 20, name: 'Daphne' }
+];
